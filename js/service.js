4d0c216a1134638d1d9d38efd0a465863e0ea34d
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-10-24 15:43:11 -0600

    fixed issues

---
+++ js/service.js
@@ -0,0 +1,30 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.service', [])
+    .service('coffeeService', coffeeService);
+
+  coffeeService.$inject = ['$http'];
+
+  function coffeeService($http) {
+    /*jshint validthis: true */
+    const baseURL = 'http://salty-harbor-81589.herokuapp.com/coffee';
+    this.getAllCoffee = function() {
+      return $http.get(baseURL);
+    };
+    this.getSingleCoffee = function(id) {
+      return $http.get(`${baseURL}${id}`);
+    };
+    this.addCoffee = function(coffee) {
+      return $http({
+        method: 'POST',
+        url: baseURL,
+        data: coffee,
+        headers: {'Content-Type': 'application/json'}
+      });
+    };
+  }
+
+})();
